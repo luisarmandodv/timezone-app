2f471e1c360b748de97e1bcd67199b3a391f3709
Armando Duran <luisarmandodv@gmail.com>
2021-01-14 02:21:55 -0600

    Refactor and improve visual experience

--- App.js
+++ App.js
@@ -1,188 +1,44 @@
-import 'react-native-gesture-handler';
-import React, {Component} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  StatusBar,
-  ActivityIndicator,
-} from 'react-native';
-import {Text, Input, Button} from 'react-native-elements';
-import ActionButton from 'react-native-action-button';
-import axios from 'axios';
-import _ from 'lodash';
-import Modal from './components/Modal';
-import TimezoneList from './components/TimezoneList';
-import {getMultiple, storeData, getTimezoneByColumn} from './utils';
+import React, {useState} from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Feather from 'react-native-vector-icons/Feather';
+import Home from './components/Home';
+import Context from './utils/Context';
 
-const WorldTimeAPI = `http://worldtimeapi.org/api/`;
+const Stack = createStackNavigator();
 
-class App extends Component {
-  state = {
-    loadingScreen: true,
-    modalVisible: false,
-    allTimezones: undefined,
-    allStoredKeys: undefined,
-  };
-
-  async componentDidMount() {
-    try {
-      const response = await axios.get(`${WorldTimeAPI}timezone`);
-      const allTimezones = response.data.map((zone) => ({
-        label: zone,
-        value: zone,
-      }));
-      await this.requestTimezones();
-
-      this.setState({
-        allTimezones,
-        loadingScreen: false,
-      });
-    } catch (error) {
-      console.log(error.response);
-    }
-  }
-
-  requestTimezones = async () => {
-    const currentUTCTimezone = await axios.get(
-      `${WorldTimeAPI}timezone/Etc/UTC`,
-    );
-    const currentTimezone = await axios.get(`${WorldTimeAPI}ip`);
-    const allStoredKeys = await getMultiple();
-    const parsedKeys = allStoredKeys.map((key) => ({
-      id: key[0],
-      ...JSON.parse(key[1]),
-    }));
-    const requestAllStoredTimezones = await Promise.all(
-      parsedKeys.map(async (item) => {
-        const response = await axios.get(
-          `${WorldTimeAPI}timezone/${item.timezone}`,
-        );
-        return {
-          ...item,
-          ...response.data,
-        };
-      }),
-    );
-    const timezonesByColumn = getTimezoneByColumn(
-      currentUTCTimezone,
-      currentTimezone,
-      requestAllStoredTimezones,
-    );
-    const allTimezonesArray = [
-      currentUTCTimezone.data,
-      currentTimezone.data,
-      ...requestAllStoredTimezones,
-    ];
-    this.setState({
-      allStoredKeys: parsedKeys,
-      timezonesByColumn,
-      allTimezonesArray,
-    });
-  };
-
-  onModalButtonPress = async (friendName, selectedTimeZone) => {
-    if (selectedTimeZone && friendName) {
-      this.setState({loadingScreen: true});
-      const stringifyObject = JSON.stringify({
-        timezone: selectedTimeZone,
-        friendName,
-      });
-      await storeData(
-        `${this.state.allStoredKeys.length + 1}`,
-        stringifyObject,
-      );
-      this.setModalVisible(false);
-      await this.requestTimezones();
-      this.setState({
-        friendName: undefined,
-        selectedTimeZone: undefined,
-        loadingScreen: false,
-      });
-    }
-  }
-
-  setModalVisible = (visible) => {
-    this.setState({modalVisible: visible});
-  };
-
-  renderModal = () => {
-    return (
-      <Modal
-        modalVisible={this.state.modalVisible}
-        allTimezones={this.state.allTimezones}
-        onCloseModal={() => {
-          this.setModalVisible(false);
-        }}
-        onPress={this.onModalButtonPress}
-      />
-    );
-  };
-
-  renderList = () => {
-    const {timezonesByColumn, allTimezonesArray} = this.state;
-    if (timezonesByColumn && timezonesByColumn.length > 0) {
-      return (
-        <TimezoneList
-          allTimezonesArray={allTimezonesArray}
-          timezonesByColumn={timezonesByColumn}
-        />
-      );
-    }
-  };
-
-  renderLoader = () => {
-    return (
-      <ActivityIndicator
-        hidesWhenStopped
-        size="large"
-        animating={this.state.loadingScreen}
-        style={styles.activityIndicator}
-      />
-    );
-  };
-
-  render() {
-    console.disableYellowBox = true;
-    return (
-      <SafeAreaView style={styles.container}>
-        <StatusBar barStyle="dark-content" />
-        {this.renderModal()}
-        <Text
-          h2
-          style={styles.header}>
-          Team Timezones
-        </Text>
-        {this.renderLoader()}
-        {this.renderList()}
-        <ActionButton
-          buttonColor="#60a3bc"
-          onPress={() => {
-            this.setModalVisible(true);
-          }}
-          position={'center'}
-        />
-      </SafeAreaView>
-    );
-  }
+function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  return (
+    <Context.Provider
+      value={{
+        isModalVisible,
+        setIsModalVisible,
+      }}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{
+              title: 'Team Timezones',
+              headerRight: (props) => (
+                <View style={{marginRight: 20}}>
+                  <Feather
+                    name="plus"
+                    size={30}
+                    color="#3c6382"
+                    onPress={() => setIsModalVisible(true)}
+                  />
+                </View>
+              ),
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Context.Provider>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {flex: 1, backgroundColor: '#f3f3f3'},
-  actionButtonIcon: {
-    fontSize: 20,
-    height: 22,
-    color: 'white',
-  },
-  header: {
-    alignSelf: 'center',
-  },
-  activityIndicator: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-  },
-});
 
 export default App;
--- components/Modal.js
+++ components/Modal.js
@@ -1,63 +1,187 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef, useReducer} from 'react';
 import {
+  TextInput,
   StyleSheet,
   View,
   TouchableOpacity,
   Modal,
   KeyboardAvoidingView,
 } from 'react-native';
-import {Text, Input, Button} from 'react-native-elements';
-import RNPickerSelect from 'react-native-picker-select';
+import {Text} from 'react-native-elements';
+import {createStackNavigator} from '@react-navigation/stack';
+import Swiper from 'react-native-swiper';
+import Entypo from 'react-native-vector-icons/Entypo';
 import _ from 'lodash';
+import SearchModal from './SearchModal';
+
+const Stack = createStackNavigator();
+
+const renderModalHeader = ({
+  leftText = null,
+  centerText,
+  rightText,
+  onLeftTextPress,
+  onRightTextPress,
+}) => {
+  const leftButton = leftText ? (
+    <TouchableOpacity
+      style={[styles.cancelContainer, {alignSelf: 'flex-start'}]}
+      onPress={onLeftTextPress}>
+      <Text style={{color: '#3c6382', fontWeight: 'bold'}}>{leftText}</Text>
+    </TouchableOpacity>
+  ) : (
+    <View style={{justifyContent: 'center', alignSelf: 'center'}}>
+      <Entypo
+        name="chevron-left"
+        size={30}
+        color="#3c6382"
+        onPress={onLeftTextPress}
+      />
+    </View>
+  );
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%',
+        backgroundColor: 'white',
+        paddingHorizontal: 20,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        shadowOpacity: 0.1,
+        shadowRadius: 1,
+        shadowOffset: {
+          height: 0,
+          width: 0,
+        },
+        elevation: 1,
+      }}>
+      {leftButton}
+      <Text style={{padding: 20, fontWeight: 'bold'}}>{centerText}</Text>
+      <TouchableOpacity
+        style={styles.cancelContainer}
+        onPress={onRightTextPress}>
+        <Text style={{color: '#3c6382', fontWeight: 'bold'}}>{rightText}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 const ModalComponent = (props) => {
   const [selectedTimeZone, setSelectedTimeZone] = useState();
   const [friendName, setFriendName] = useState();
-  const {modalVisible, onCloseModal, onPress, allTimezones} = props;
+  const {isModalVisible, onCloseModal, onPress, allTimezones} = props;
+  const SwiperRef = useRef(null);
 
   return (
     <Modal
       animationType="slide"
-      visible={modalVisible}
+      visible={isModalVisible}
       presentationStyle={'pageSheet'}
       transparent>
-      <KeyboardAvoidingView
-        keyboardVerticalOffset={-100}
-        behavior={'height'}
-        style={styles.modalContainer}>
+      <Swiper
+        ref={SwiperRef}
+        style={{}}
+        showsPagination={false}
+        scrollEnabled={false}
+        showsButtons={false}>
+        <KeyboardAvoidingView
+          keyboardVerticalOffset={-100}
+          behavior={'height'}
+          style={styles.modalContainer}>
+          <View style={styles.container}>
+            {renderModalHeader({
+              leftText: 'Cancel',
+              centerText: 'Add Timezone',
+              rightText: 'Save',
+              onLeftTextPress: () => {
+                setSelectedTimeZone(undefined);
+                setFriendName(undefined);
+                onCloseModal();
+              },
+              onRightTextPress: () => {
+                onPress(friendName, selectedTimeZone);
+              },
+            })}
+            <View
+              style={{
+                paddingTop: 20,
+                flex: 1,
+                alignSelf: 'stretch',
+              }}>
+              <View
+                style={{
+                  alignSelf: 'stretch',
+                  backgroundColor: 'white',
+                }}>
+                <View
+                  style={{
+                    height: 40,
+                    justifyContent: 'center',
+                    paddingHorizontal: 25,
+                  }}>
+                  <TextInput
+                    placeholder="Name"
+                    onEndEditing={(value) => {
+                      setFriendName(value.nativeEvent.text);
+                    }}
+                    placeholderTextColor={'#C7C7CD'}
+                  />
+                </View>
+                <View
+                  style={{
+                    marginLeft: 25,
+                    height: StyleSheet.hairlineWidth,
+                    width: '100%',
+                    backgroundColor: '#e1e8ee',
+                  }}
+                />
+                <View
+                  style={{
+                    alignSelf: 'stretch',
+                    backgroundColor: 'white',
+                    height: 40,
+                    justifyContent: 'center',
+                    paddingHorizontal: 25,
+                  }}>
+                  <TouchableOpacity
+                    style={{
+                      flexDirection: 'row',
+                      justifyContent: 'space-between',
+                      alignItems: 'center',
+                    }}
+                    onPress={() => SwiperRef.current.scrollBy(1, true)}>
+                    {selectedTimeZone ? (
+                      <Text>{selectedTimeZone}</Text>
+                    ) : (
+                      <Text style={{color: '#C7C7CD'}}>Search Timezone</Text>
+                    )}
+                    <Entypo name="chevron-right" size={20} color="#3c6382" />
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          </View>
+        </KeyboardAvoidingView>
         <View style={styles.container}>
-          <TouchableOpacity
-            style={styles.cancelContainer}
-            onPress={() => {
-              setSelectedTimeZone(undefined);
-              setFriendName(undefined);
-              onCloseModal();
-            }}>
-            <Text style={{color: '#82ccdd', fontWeight: 'bold'}}>Cancel</Text>
-          </TouchableOpacity>
-          <Input
-            placeholder="Name"
-            onChangeText={(value) => {
-              setFriendName(value);
-            }}
-          />
-          <RNPickerSelect
-            items={allTimezones}
-            onValueChange={(value) => {
+          {renderModalHeader({
+            centerText: 'Search Timezone',
+            rightText: '',
+            onLeftTextPress: () => {
+              SwiperRef.current.scrollBy(1, false);
+            },
+            onRightTextPress: () => {},
+          })}
+          <SearchModal
+            allTimezones={allTimezones}
+            onPress={(value) => {
               setSelectedTimeZone(value);
+              SwiperRef.current.scrollBy(1, false);
             }}
-            style={{
-              ...pickerSelectStyles,
-            }}
-            value={selectedTimeZone}
-          />
-          <Button
-            title="Add New Member"
-            onPress={() => onPress(friendName, selectedTimeZone)}
-            containerStyle={styles.buttonContainerStyle}
           />
         </View>
-      </KeyboardAvoidingView>
+      </Swiper>
     </Modal>
   );
 };
@@ -67,12 +191,8 @@ const styles = StyleSheet.create({
   container: {
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
-    height: 400,
-    width: '100%',
-    position: 'absolute',
-    bottom: 0,
-    backgroundColor: 'white',
-    paddingHorizontal: 20,
+    flex: 1,
+    backgroundColor: '#F3F3F3',
     alignItems: 'center',
   },
   cancelContainer: {
@@ -80,14 +200,6 @@ const styles = StyleSheet.create({
     padding: 20,
     elevation: 2,
     alignSelf: 'flex-end',
-  },
-  buttonContainerStyle: {
-    position: 'absolute',
-    bottom: 100,
-    marginTop: 20,
-    width: '60%',
-    marginHorizontal: 20,
-    borderRadius: 20,
   },
 });
 
